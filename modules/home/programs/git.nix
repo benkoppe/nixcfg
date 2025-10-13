d7359181f197ff2808945b21c8149dc4198bf1da
Ben <koppe.development@gmail.com>
2025-10-13 01:32:49 -0400

    Correct and consolidate git singing key config

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -5,9 +5,6 @@
   self,
   ...
 }:
-let
-  githubPublicSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqe4FEfKED0fJ1IETiws0aYV1lzDTBuGJfBFi+WTsJ8 ben@Bens-MBP";
-in
 {
   options.myHome.programs.git = {
     enable = lib.mkEnableOption "git";
@@ -37,17 +34,28 @@ in
         };
       }
 
-      (lib.mkIf config.myHome.programs.git.signingKey.enable {
-        programs.git.extraConfig = {
-          commit.gpgsign = true;
-          gpg.format = "ssh";
-          user.signingKey = config.age.secrets."github-signing-key".path;
-        };
+      (lib.mkIf config.myHome.programs.git.signingKey.enable (
+        let
+          signingKeyPath = ".ssh/github_sign";
+          githubPublicSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqe4FEfKED0fJ1IETiws0aYV1lzDTBuGJfBFi+WTsJ8 ben@Bens-MBP";
+        in
+        {
+          programs.git.extraConfig = {
+            commit.gpgsign = true;
+            gpg.format = "ssh";
+            user.signingKey = "~/${signingKeyPath}";
+          };
 
-        age.secrets."github-signing-key".file = "${self.inputs.secrets}/github-signing-key.age";
+          age.secrets."github-signing-key" = {
+            file = "${self.inputs.secrets}/github-signing-key.age";
+            symlink = false;
+            path = "$HOME/${signingKeyPath}";
+            mode = "600";
+          };
 
-        home.file.".ssh/github_sign.pub".text = githubPublicSigningKey;
-      })
+          home.file.".ssh/github_sign.pub".text = githubPublicSigningKey;
+        }
+      ))
     ]
   );
 }
