2c201d15ff72f8e6134671aeede91c08e0ad6edf
Ben <koppe.development@gmail.com>
2025-10-15 10:16:31 -0400

    Add enableDefaultConfig equivalent and unused enableServers ssh option

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -5,23 +5,50 @@
   ...
 }:
 {
-  options.myHome.programs.ssh.enable = lib.mkEnableOption "openssh client";
+  options.myHome.programs.ssh = {
+    enable = lib.mkEnableOption "openssh client";
 
-  config = lib.mkIf config.myHome.programs.ssh.enable {
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-
-      package = pkgs.openssh;
-
-      matchBlocks = {
-        "github.com" = {
-          identitiesOnly = true;
-          identityFile = [
-            "~/.ssh/id_github"
-          ];
-        };
-      };
-    };
+    enableServers = lib.mkEnableOption "reach main servers via ssh";
   };
+
+  config = lib.mkIf config.myHome.programs.ssh.enable (
+    lib.mkMerge [
+      {
+        programs.ssh = {
+          enable = true;
+          enableDefaultConfig = false;
+
+          package = pkgs.openssh;
+
+          matchBlocks = {
+            "github.com" = {
+              identitiesOnly = true;
+              identityFile = [
+                "~/.ssh/id_github"
+              ];
+            };
+          };
+
+          # equivalent to enableDefaultConfig = true;
+          matchBlocks."*" = {
+            forwardAgent = false;
+            addKeysToAgent = "no";
+            compression = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            hashKnownHosts = false;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
+          };
+        };
+      }
+
+      (lib.mkIf config.myHome.programs.ssh.enableServers {
+        programs.ssh = {
+        };
+      })
+    ]
+  );
 }
