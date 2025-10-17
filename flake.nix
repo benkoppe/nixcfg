1797b08d43eac2e8a6121c91b8cc55f34b312bcf
Ben <koppe.development@gmail.com>
2025-10-17 15:29:40 -0400

    Add install.determinate.systems to extra-substituters

--- flake.nix
+++ flake.nix
@@ -58,11 +58,12 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cache.nixos.org"
+      "https://install.determinate.systems"
     ];
-
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
     ];
   };
 
