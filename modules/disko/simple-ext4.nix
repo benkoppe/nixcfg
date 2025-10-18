250c6541c2230c3812f201c164698a695911f80c
Ben <koppe.development@gmail.com>
2025-10-18 13:39:38 -0400

    Shift around base disko ext4 config

--- modules/disko/simple-ext4.nix
+++ modules/disko/simple-ext4.nix
@@ -26,25 +26,30 @@
           type = "gpt";
           partitions = {
             boot = {
+              name = "boot";
               size = "1M";
               type = "EF02"; # for grub MBR
             };
-            ESP = {
+            esp = {
+              name = "ESP";
               size = "1G";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
+              name = "root";
               size = "100%";
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
+                mountOptions = [
+                  "defaults"
+                ];
               };
             };
           };
