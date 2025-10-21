d227da4ddd31f30d0b925d9d5fe9d27289f1626d
Ben <koppe.development@gmail.com>
2025-10-20 23:49:43 -0400

    Relocate nixos settings to base profile

--- hosts/russ/default.nix
+++ hosts/russ/default.nix
@@ -1,7 +1,5 @@
 {
-  config,
   self,
-  pkgs,
   modulesPath,
   ...
 }:
@@ -15,25 +13,6 @@
 
   myNixOS = {
     profiles.base.enable = true;
-  };
-
-  nix.settings = {
-    sandbox = false;
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org"
-      "https://install.determinate.systems"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-    ];
-    trusted-users = [
-      "root"
-      "@wheel"
-      "builder"
-    ];
   };
 
   nixpkgs.hostPlatform = "x86_64-linux";
