5e178d06b488b35d231e821cafa5b89657232046
Ben <koppe.development@gmail.com>
2025-10-21 00:29:24 -0400

    Add specific identity for builder-1-root access

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -57,8 +57,16 @@
 
             "builder-1" = {
               hostname = "10.192.168.240";
+              user = "builder";
               identitiesOnly = true;
               identityFile = config.age.secrets.ssh-builder-1.path;
+            };
+
+            "builder-1-root" = {
+              hostname = "10.192.168.240";
+              user = "root";
+              identitiesOnly = true;
+              identityFile = config.age.secrets.ssh-builder-1-root.path;
             };
           };
         };
@@ -75,6 +83,13 @@
             file = "${self.inputs.secrets}/pve/builder-1-key.age";
             symlink = false;
             path = "${config.home.homeDirectory}/.ssh/pve/builder-1";
+            mode = "600";
+          };
+
+          ssh-builder-1-root = {
+            file = "${self.inputs.secrets}/pve/builder-1-root-key.age";
+            symlink = false;
+            path = "${config.home.homeDirectory}/.ssh/pve/builder-1-root";
             mode = "600";
           };
         };
