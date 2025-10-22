17a7fc9c82cef4f5dd02e6e88568ff04b9ba9733
Ben <koppe.development@gmail.com>
2025-10-22 15:25:38 -0400

    Extract nixos nix config to dedicated file

--- modules/nixos/profiles/base.nix
+++ modules/nixos/profiles/base.nix
@@ -22,23 +22,8 @@
   options.myNixOS.profiles.base.enable = lib.mkEnableOption "base system configuration";
 
   config = lib.mkIf config.myNixOS.profiles.base.enable {
-    nix.settings = {
-      sandbox = false;
-      extra-substituters = [
-        "https://nix-community.cachix.org"
-        "https://cache.nixos.org"
-        "https://install.determinate.systems"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-      ];
-      trusted-users = [
-        "root"
-        "@wheel"
-        "builder"
-      ];
+    myNixOS = {
+      programs.nix.enable = true;
     };
 
     home-manager = {
---
+++ modules/nixos/programs/nix.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  lib,
+  self,
+  ...
+}:
+let
+  isBuildMachine =
+    let
+      buildHosts = lib.map (m: m.hostName) config.mySnippets.nix.buildMachines;
+    in
+    lib.elem config.networking.hostName buildHosts;
+in
+{
+  options.myNixOS.programs.nix.enable = lib.mkEnableOption "sane nix configuration";
+
+  config = lib.mkIf config.myNixOS.programs.nix.enable {
+    nix = {
+      buildMachines = lib.mkIf config.services.tailscale.enable (
+        lib.filter (m: m.hostName != config.networking.hostName) config.mySnippets.nix.buildMachines
+      );
+
+      distributedBuilds = true;
+
+      gc = {
+        automatic = true;
+
+        options = if isBuildMachine then "--delete-older-than 20d" else "--delete-older-than 3d";
+
+        persistent = true;
+        randomizedDelaySec = "60min";
+      };
+
+      extraOptions = ''
+        min-free = ${toString (1 * 1024 * 1024 * 1024)}   # 1 GiB
+        max-free = ${toString (5 * 1024 * 1024 * 1024)}   # 5 GiB
+      '';
+
+      optimise = {
+        automatic = true;
+        persistent = true;
+        randomizedDelaySec = "60min";
+      };
+
+      inherit (config.mySnippets.nix) settings;
+    };
+  };
+}
