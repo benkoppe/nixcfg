6e3c85609896ee2d5396f32ebe6ec35530310c61
Ben <koppe.development@gmail.com>
2025-10-30 16:28:17 -0400

    Use basedpyright in lazyvim config

--- modules/home/programs/nvim/lua/config/options.lua
+++ modules/home/programs/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Some default options are always set: <DOCS_LINK>
 -- Add any additional options here
 vim.g.snacks_animate = false
+
+-- Use basedpyright with python
+vim.g.lazyvim_python_lsp = "basedpyright"
