c1cbbe8bc45ded57fb139906874d72c461788632
Ben <koppe.development@gmail.com>
2025-11-21 16:13:17 -0500

    Increase nix-builder ct resources

--- hosts/nix-builder/terranix.nix
+++ hosts/nix-builder/terranix.nix
@@ -1,3 +1,14 @@
 {
-  myTerranix.profiles.proxmox-lxc.enable = true;
+  myTerranix.profiles.proxmox-lxc = {
+    enable = true;
+
+    disk.size = 50;
+
+    cpu.cores = 10;
+
+    memory = {
+      dedicated = 32768;
+      swap = 4096;
+    };
+  };
 }
