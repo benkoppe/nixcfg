55229431c71dc31d200cb5c108d11a9b59e0b791
Ben <koppe.development@gmail.com>
2025-11-23 00:15:05 -0500

    Add nginx service

---
+++ modules/nixos/services/nginx.nix
@@ -0,0 +1,83 @@
+{
+  lib,
+  config,
+  self,
+  ...
+}:
+{
+  options.myNixOS.services.nginx = {
+    enable = lib.mkEnableOption "Nginx web server";
+
+    domain = lib.mkOption {
+      type = lib.types.str;
+      description = "The domain name for the Nginx server.";
+      default = "thekoppe.com";
+    };
+
+    subdomain = lib.mkOption {
+      type = lib.types.str;
+      description = "The subdomain for the Caddy server.";
+    };
+
+    port = lib.mkOption {
+      type = lib.types.int;
+      description = "The port Nginx should forward.";
+    };
+
+    networkDevice = lib.mkOption {
+      type = lib.types.str;
+      description = "The network device to bind to.";
+      default = config.mySnippets.networks.tailscale.deviceName;
+    };
+
+    proxyWebsockets = lib.mkEnableOption "Also proxy websockets";
+  };
+
+  config =
+    let
+      cfg = config.myNixOS.services.nginx;
+    in
+    lib.mkIf cfg.enable {
+      security.acme = {
+        acceptTerms = true;
+        defaults.email = "koppe.development@gmail.com";
+
+        certs."${cfg.subdomain}.${cfg.domain}" = {
+          inherit (config.services.nginx) group;
+          webroot = null;
+          dnsProvider = "cloudflare";
+          dnsResolver = "1.1.1.1:53";
+          dnsPropagationCheck = true;
+          environmentFile = config.age.secrets.caddy-cloudflare.path;
+        };
+      };
+
+      age.secrets.caddy-cloudflare.file = "${self.inputs.secrets}/services/caddy/cloudflare-api.age";
+
+      services.nginx = {
+        enable = true;
+
+        recommendedGzipSettings = true;
+        recommendedOptimisation = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
+
+        virtualHosts = {
+          "${cfg.subdomain}.${cfg.domain}" = {
+            enableACME = true;
+            forceSSL = true;
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:${toString cfg.port}";
+              inherit (cfg) proxyWebsockets;
+            };
+          };
+        };
+      };
+
+      networking.firewall.interfaces = {
+        ${cfg.networkDevice}.allowedTCPPorts = [
+          443
+        ];
+      };
+    };
+}
