9d5ac8cf6a78cf7d3827921e6d8961c413dc3a05
ben <koppe.development@gmail.com>
2025-11-28 16:02:02 -0500

    Hercules-ci: Move runColmena deploy output to onPush.default

--- modules/flake/hercules-ci.nix
+++ modules/flake/hercules-ci.nix
@@ -9,44 +9,44 @@
       "aarch64-linux"
       "x86_64-linux"
     ];
+
+    onPush.default.outputs = withSystem "x86_64-linux" (
+      {
+        hci-effects,
+        pkgs,
+        ...
+      }:
+      let
+        runColmena = hci-effects.mkEffect {
+          inputs = [
+            self.inputs.colmena.packages.${pkgs.stdenv.hostPlatform.system}.colmena
+            self.inputs.determinate-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
+            pkgs.openssh
+          ]
+          ++ (map (host: self.nixosConfigurations.${host}.config.system.build.toplevel) [
+            "russ"
+            "nix-builder"
+            "adguard"
+            "lldap"
+            "pocket-id"
+            "vaultwarden"
+            "immich"
+            "forgejo"
+            "forgejo-runner"
+            "garage-dray"
+            "komodo"
+          ]);
+
+          secretsMap.ssh = "colmena-ssh";
+
+          userSetupScript = "writeSSHKey ssh";
+
+          effectScript = "colmena apply --config ${self.outPath}/flake.nix --nix-option accept-flake-config true";
+        };
+      in
+      {
+        deploy = runColmena;
+      }
+    );
   };
-
-  flake.effects = withSystem "x86_64-linux" (
-    {
-      hci-effects,
-      pkgs,
-      ...
-    }:
-    let
-      runColmena = hci-effects.mkEffect {
-        inputs = [
-          self.inputs.colmena.packages.${pkgs.stdenv.hostPlatform.system}.colmena
-          self.inputs.determinate-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
-          pkgs.openssh
-        ]
-        ++ (map (host: self.nixosConfigurations.${host}.config.system.build.toplevel) [
-          "russ"
-          "nix-builder"
-          "adguard"
-          "lldap"
-          "pocket-id"
-          "vaultwarden"
-          "immich"
-          "forgejo"
-          "forgejo-runner"
-          "garage-dray"
-          "komodo"
-        ]);
-
-        secretsMap.ssh = "colmena-ssh";
-
-        userSetupScript = "writeSSHKey ssh";
-
-        effectScript = "colmena apply --config ${self.outPath}/flake.nix --nix-option accept-flake-config true";
-      };
-    in
-    {
-      deploy = runColmena;
-    }
-  );
 }
