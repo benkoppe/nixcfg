b3558fbb14fbb6bfbb6c03a8322fe15a915940f7
ben <koppe.development@gmail.com>
2025-11-30 19:26:38 -0500

    Adguard: consolidate config and enable home network

--- hosts/adguard/snippets.nix
+++ hosts/adguard/snippets.nix
@@ -1,9 +1,10 @@
 { config, ... }:
 let
-  inherit (config.mySnippets.networks) tailscale;
+  inherit (config.mySnippets.networks) tailscale home;
   vm_id = 245;
 in
 {
   inherit vm_id;
   ipv4 = "${tailscale.prefix}.${toString vm_id}";
+  home_ipv4 = "${home.prefix}.102";
 }
--- hosts/adguard/terranix.nix
+++ hosts/adguard/terranix.nix
@@ -1,3 +1,28 @@
+{ config, ... }:
+let
+  inherit (config.mySnippets) networks hostName;
+  inherit (config.mySnippets.hosts.${hostName}) ipv4 home_ipv4;
+in
 {
-  myTerranix.profiles.proxmox-lxc.enable = true;
+  myTerranix.profiles.proxmox-lxc = {
+    enable = true;
+
+    networks = {
+      tailscale = {
+        inherit (networks.tailscale) bridge deviceName;
+        ipv4 = {
+          inherit (networks.tailscale) gateway;
+          address = ipv4;
+        };
+      };
+
+      home = {
+        inherit (networks.home) bridge deviceName;
+        ipv4 = {
+          inherit (networks.home) gateway;
+          address = home_ipv4;
+        };
+      };
+    };
+  };
 }
