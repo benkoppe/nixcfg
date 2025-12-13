c8d08a23f846499bf5956dbc3a136e6b46421d1b
ben <koppe.development@gmail.com>
2025-12-13 17:40:27 -0500

    Cloudflared-dray: add komodo hosts

--- hosts/cloudflared-dray/default.nix
+++ hosts/cloudflared-dray/default.nix
@@ -1,4 +1,9 @@
-{ config, inputs, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 {
   myNixOS = {
     profiles.proxmox-lxc.enable = true;
@@ -17,6 +22,16 @@
           address = "10.1.1.21";
           port = 1411;
         }
+        {
+          vHost = "komodo2.thekoppe.com";
+          address = "10.192.168.90";
+          port = 9120;
+        }
+        {
+          vHost = "komodo.thekoppe.com";
+          address = "10.0.0.210";
+          port = 9120;
+        }
       ];
     };
   };
@@ -27,16 +42,19 @@
       "ba89c3b8-1df3-4d31-9030-54e8475c3c25" = {
         credentialsFile = config.age.secrets.cloudflared-dray.path;
         default = "http_status:404";
-        ingress = {
-          "git.thekoppe.com" = {
+        ingress =
+          let
+            hosts = [
+              "git.thekoppe.com"
+              "pocket.thekoppe.com"
+              "komodo2.thekoppe.com"
+              "komodo.thekoppe.com"
+            ];
+          in
+          lib.genAttrs hosts (host: {
             service = "https://localhost:443";
-            originRequest.originServerName = "git.thekoppe.com";
-          };
-          "pocket.thekoppe.com" = {
-            service = "https://localhost:443";
-            originRequest.originServerName = "pocket.thekoppe.com";
-          };
-        };
+            originRequest.originServerName = host;
+          });
       };
     };
   };
