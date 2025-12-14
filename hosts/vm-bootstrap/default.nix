5ca1f55be6de68e7a6314e5edb338d33b618c578
ben <koppe.development@gmail.com>
2025-12-14 12:48:25 -0500

    Nixos: update vm-bootstrap

--- hosts/vm-bootstrap/default.nix
+++ hosts/vm-bootstrap/default.nix
@@ -1,8 +1,17 @@
 {
   inputs,
+  self,
+  modulesPath,
   ...
 }:
 {
+  imports = [
+    self.diskoConfigurations.simple-ext4
+    (modulesPath + "/profiles/qemu-guest.nix")
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ./hardware-configuration.nix
+  ];
+
   myNixOS = {
     profiles.server.colmenaSshAccess.enable = false;
 
@@ -16,4 +25,23 @@
 
     hashedPassword = "";
   };
+
+  services.qemuGuest.enable = true;
+
+  boot.loader = {
+    grub = {
+      enable = true;
+      # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+      # devices = [ ];
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+    };
+  };
+
+  services.cloud-init = {
+    enable = true;
+    network.enable = true;
+  };
+
+  networking.useDHCP = false; # important (allegedly)
 }
