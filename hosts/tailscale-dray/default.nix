1f05a1c70a23be7ceb98fe80a78fdbc5c32a42c3
ben <koppe.development@gmail.com>
2025-12-15 13:18:39 -0500

    Tailscale-dray: enable caddy service

--- hosts/tailscale-dray/default.nix
+++ hosts/tailscale-dray/default.nix
@@ -4,6 +4,35 @@
     profiles.proxmox-vm.enable = true;
 
     services.tailscale-server.enable = true;
+
+    services.caddy = {
+      enable = true;
+
+      virtualHosts = [
+        {
+          vHost = "pve.thekoppe.com";
+          address = "https://10.192.168.1";
+          port = 8006;
+          insecureTLS = true;
+        }
+        {
+          vHost = "pbs.thekoppe.com";
+          address = "https://10.192.168.130";
+          port = 8007;
+          insecureTLS = true;
+        }
+        {
+          vHost = "files.thekoppe.com";
+          address = "10.192.168.204";
+          port = 8081;
+        }
+        {
+          vHost = "office.thekoppe.com";
+          address = "10.192.168.204";
+          port = 8080;
+        }
+      ];
+    };
   };
 
   networking =
