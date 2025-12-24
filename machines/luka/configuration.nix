5e43463c712f1b749705908ed4302cadd5e6bebe
ben <koppe.development@gmail.com>
2025-12-24 03:41:43 -0800

    Add luks password to luka vars

--- machines/luka/configuration.nix
+++ machines/luka/configuration.nix
@@ -3,4 +3,16 @@
   imports = with self.nixosModules; [
     nix
   ];
+
+  clan.core.vars.generators.luks-password = {
+    prompts.password = {
+      description = "LUKS password for machine luka";
+      type = "hidden";
+    };
+    files.password = {
+      secret = true;
+    };
+
+    script = "cp $prompts/password $out/password";
+  };
 }
