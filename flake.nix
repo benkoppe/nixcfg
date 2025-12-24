f9c45e9410d1959018cc25b1491d94f59a49eb57
ben <koppe.development@gmail.com>
2025-12-23 19:03:59 -0800

    Luka: add rudimentary plasma-manager config

--- flake.nix
+++ flake.nix
@@ -70,6 +70,12 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
     nvim-flake = {
       url = "github:benkoppe/nvim-flake";
       inputs.nixpkgs.follows = "nixpkgs";
