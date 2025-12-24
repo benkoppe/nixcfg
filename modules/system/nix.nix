6d86a3197bddbaa080912d6a805586f747d7c818
ben <koppe.development@gmail.com>
2025-12-24 01:56:38 -0800

    Move all imports to top-level

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,37 +1,36 @@
+{ inputs, lib, ... }:
 {
-  config.flake.nixosModules.nix =
-    { lib, inputs, ... }:
-    {
-      nix = {
-        gc = {
-          automatic = true;
-          options = "--delete-older-than 3d";
-          persistent = true;
-          randomizedDelaySec = "60min";
-        };
+  flake.modules.nixos.nix = {
+    nix = {
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 3d";
+        persistent = true;
+        randomizedDelaySec = "60min";
+      };
 
-        # run GC when there is less than min-free space until there is max-free space
-        extraOptions = ''
-          min-free = ${toString (1 * 1024 * 1024 * 1024)} # 1 GiB
-          max-free = ${toString (5 * 1024 * 1024 * 1024)} # 5 GiB
-        '';
+      # run GC when there is less than min-free space until there is max-free space
+      extraOptions = ''
+        min-free = ${toString (1 * 1024 * 1024 * 1024)} # 1 GiB
+        max-free = ${toString (5 * 1024 * 1024 * 1024)} # 5 GiB
+      '';
 
-        optimise = {
-          automatic = true;
-          persistent = true;
-          randomizedDelaySec = "60min";
-        };
+      optimise = {
+        automatic = true;
+        persistent = true;
+        randomizedDelaySec = "60min";
+      };
 
+    }
+    // (
+      let
+        registryMap = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
+      in
+      {
+        channel.enable = false;
+        registry = lib.mapAttrs (_: flake: { inherit flake; }) registryMap;
+        nixPath = lib.mapAttrsToList (name: flake: "${name}=${flake.outPath}") registryMap;
       }
-      // (
-        let
-          registryMap = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
-        in
-        {
-          channel.enable = false;
-          registry = lib.mapAttrs (_: flake: { inherit flake; }) registryMap;
-          nixPath = lib.mapAttrsToList (name: flake: "${name}=${flake.outPath}") registryMap;
-        }
-      );
-    };
+    );
+  };
 }
