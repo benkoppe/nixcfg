a26c0b7f449db19c1c7e85efa4fa313da3ebaa90
ben <koppe.development@gmail.com>
2025-12-26 07:51:26 -0800

    Manage oci login using clan secrets

--- modules/terranix/modules/providers/oracle.nix
+++ modules/terranix/modules/providers/oracle.nix
@@ -1,33 +1,43 @@
 {
   flake.modules.terranix.oracle =
-    { lib, config, ... }:
     {
-      terraform.required_providers.oci = {
-        source = "oracle/oci";
-        version = "7.29.0";
-      };
-
-      provider.oci = lib.genAttrs [ "region" "tenancy_ocid" "user_ocid" "fingerprint" "private_key" ] (
-        _key: lib.tfRef "var.${config.my.key}"
-      );
-
-      variable =
-        let
-          var = description: {
-            type = "string";
-            sensitive = true;
-            inherit description;
-          };
-        in
-        {
-          region = var "OCI region";
-          tenancy_ocid = var "OCI tenancy OCID";
-          user_ocid = var "OCI user OCID";
-          fingerprint = var "OCI key pair fingerprint";
-          private_key = var "OCI key pair private key";
-
-          namespace = var "OCI Object Storage namespace";
-          compartment_ocid = var "OCI compartment OCID";
+      lib,
+      pkgs,
+      config,
+      ...
+    }:
+    (lib.mkMerge [
+      {
+        terraform.required_providers.oci = {
+          source = "oracle/oci";
+          version = "7.29.0";
         };
-    };
+      }
+
+      (lib.genAttrs
+        [
+          "region"
+          "tenancy_ocid"
+          "user_ocid"
+          "fingerprint"
+          "private_key"
+        ]
+        (key: {
+          data.external.${key} = {
+            program = [
+              (lib.getExe (
+                pkgs.writeShellApplication {
+                  name = "get-clan-secret";
+                  text = ''
+                    jq -n --arg secret "$(clan secrets get oracle-${key})" '{"secret":$secret}'
+                  '';
+                }
+              ))
+            ];
+          };
+          provider.oci.${key} = config.data.external.${key} "result.secret";
+        })
+      )
+    ]);
+
 }
