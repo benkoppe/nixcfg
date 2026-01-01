32a5a76e1a803c3d89d812b19814b4a144f83d12
ben <koppe.development@gmail.com>
2025-12-31 23:01:59 -0800

    Add service-vms helper module

---
+++ modules/system/microvms/host/service-vms.nix
@@ -0,0 +1,44 @@
+{ self, lib, ... }:
+let
+  inherit (lib) mkOption types;
+in
+{
+  flake.modules.nixos."microvms_host_service-vms" =
+    { config, ... }:
+    {
+      options.my.service-vms = mkOption {
+        type = types.attrsOf (
+          types.submodule {
+            options = {
+              id = mkOption {
+                type = types.int;
+                description = "Unique VM identifier";
+              };
+              modules = mkOption {
+                type = types.listOf types.anything;
+                default = [ ];
+                description = "List of nixos modules to include in the vm.";
+              };
+            };
+          }
+        );
+      };
+
+      config = {
+        microvm.vms = lib.mapAttrs (_name: cfg: {
+          pkgs = null;
+
+          config = {
+            imports =
+              with self.modules.nixos;
+              [
+                microvms_client
+              ]
+              ++ cfg.modules;
+
+            my.microvm.id = cfg.id;
+          };
+        }) config.my.service-vms;
+      };
+    };
+}
