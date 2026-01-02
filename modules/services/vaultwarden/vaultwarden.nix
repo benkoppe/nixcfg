8a55611a16557c7d63ceb5540df3e239fdbfb68d
ben <koppe.development@gmail.com>
2026-01-02 08:46:11 -0800

    Working vaultwarden with secrets

--- modules/services/vaultwarden/vaultwarden.nix
+++ modules/services/vaultwarden/vaultwarden.nix
@@ -14,6 +14,7 @@ in
             type = "hidden";
           };
           files.password-hash.secret = true;
+          files.password-hash.owner = "microvm";
           script = ''cat $prompts/password-input | argon2 "$(openssl rand -base64 32)" -e -id -k 65540 -t 3 -p 4 > $out/password-hash'';
           share = true;
           runtimeInputs = with pkgs; [
@@ -27,35 +28,19 @@ in
             persist = true;
             type = "hidden";
           };
+          files.password.owner = "microvm";
           share = true;
         };
       };
 
       my.service-vms.vaultwarden.modules =
         let
-          adminPass = config.clan.core.vars.generators.vaultwarden-admin.files.password-hash.path;
-          adminPassMnt = "/run/secrets/adminPass";
-          smtpPass = config.clan.core.vars.generators.koppe-development-smtp.files.password.path;
-          smtpPassMnt = "/run/secrets/smtpPass";
+          cfg = config;
         in
         [
           (
             { config, ... }:
             {
-              microvm.shares = [
-                {
-                  source = builtins.dirOf adminPass;
-                  mountPoint = adminPassMnt;
-                  tag = "adminPass";
-                  proto = "virtiofs";
-                }
-                {
-                  source = builtins.dirOf smtpPass;
-                  mountPoint = smtpPassMnt;
-                  tag = "smtpPass";
-                  proto = "virtiofs";
-                }
-              ];
               microvm.volumes = [
                 {
                   image = "vaultwarden-data.img";
@@ -63,7 +48,21 @@ in
                   size = 64;
                 }
               ];
+              microvm.credentialFiles = {
+                ADMIN_TOKEN = cfg.clan.core.vars.generators.vaultwarden-admin.files.password-hash.path;
+                SMTP_PASS = cfg.clan.core.vars.generators.koppe-development-smtp.files.password.path;
+              };
 
+              systemd.services.vaultwarden = {
+                serviceConfig.ImportCredential = [
+                  "ADMIN_TOKEN"
+                  "SMTP_PASS"
+                ];
+                environment = {
+                  ADMIN_TOKEN_FILE = "%d/ADMIN_TOKEN";
+                  SMTP_PASSWORD_FILE = "%d/SMTP_PASS";
+                };
+              };
               services.vaultwarden = {
                 enable = true;
                 dbBackend = "sqlite";
@@ -72,8 +71,6 @@ in
                   DOMAIN = "https://${vHost}";
                   SIGNUPS_ALLOWED = false;
                   DATA_FOLDER = dataDir;
-
-                  ADMIN_TOKEN_FILE = adminPassMnt;
 
                   ROCKET_ADDRESS = "127.0.0.1";
                   ROCKET_PORT = 8222;
@@ -85,7 +82,6 @@ in
                   SMTP_FROM = "koppe.development@gmail.com";
                   SMTP_USERNAME = "koppe.development@gmail.com";
                   SMTP_FROM_NAME = "Koppe Vaultwarden";
-                  SMTP_PASSWORD_FILE = smtpPassMnt;
                 };
               };
             }
