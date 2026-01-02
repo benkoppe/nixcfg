42a0f1ee0eb317904efcb47096daea19b08d1282
ben <koppe.development@gmail.com>
2026-01-01 18:30:16 -0800

    Correctly mount tailscale secret

--- modules/services/tailgate.nix
+++ modules/services/tailgate.nix
@@ -10,24 +10,38 @@
         share = true;
       };
 
-      my.service-vms.tailgate.modules = [
-        {
-          services.tailscale = {
-            enable = true;
-            openFirewall = true;
-
-            authKeyFile = config.clan.core.vars.generators.tailgate-auth-key.files.key.path;
-            useRoutingFeatures = "server";
-
-            extraSetFlags = [
-              "--ssh"
-              "--advertise-exit-node"
-              "--advertise-routes=10.0.0.0/24"
+      my.service-vms.tailgate.modules =
+        let
+          authKey = config.clan.core.vars.generators.tailgate-auth-key.files.key.path;
+          authKeyMnt = "/run/secrets/authKey";
+        in
+        [
+          {
+            microvm.shares = [
+              {
+                source = builtins.dirOf authKey;
+                mountPoint = authKeyMnt;
+                tag = "authKey";
+                proto = "virtiofs";
+              }
             ];
-          };
 
-          networking.firewall.allowedTCPPorts = [ 8088 ]; # tailscale web
-        }
-      ];
+            services.tailscale = {
+              enable = true;
+              openFirewall = true;
+
+              authKeyFile = "${authKeyMnt}/key";
+              useRoutingFeatures = "server";
+
+              extraSetFlags = [
+                "--ssh"
+                "--advertise-exit-node"
+                "--advertise-routes=10.0.0.0/24"
+              ];
+            };
+
+            networking.firewall.allowedTCPPorts = [ 8088 ]; # tailscale web
+          }
+        ];
     };
 }
