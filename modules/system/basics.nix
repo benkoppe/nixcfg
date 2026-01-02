88fcd602566284901cb8d1cc8f463295fc7d6411
ben <koppe.development@gmail.com>
2026-01-01 18:30:21 -0800

    Add :q shell alias to basic

--- modules/system/basics.nix
+++ modules/system/basics.nix
@@ -4,5 +4,9 @@
     imports = with self.modules.nixos; [ nix ];
 
     time.timeZone = lib.mkDefault "America/Los_Angeles";
+
+    programs.bash.shellAliases = {
+      ":q" = "exit";
+    };
   };
 }
