f9d97f863195adeeba01d36c180354ad44f00d23
ben <koppe.development@gmail.com>
2026-01-02 16:01:52 -0800

    Working vaultwarden

--- modules/services/microvms/vaultwarden/vaultwarden.nix
+++ modules/services/microvms/vaultwarden/vaultwarden.nix
@@ -1,12 +1,15 @@
+{ self, ... }:
 let
   dataDir = "/var/lib/vaultwarden/data";
 
-  vHost = "vault.thekoppe.com";
+  vHost = "vault2.thekoppe.com";
 in
 {
   flake.modules.nixos.vaultwarden =
-    { config, pkgs, ... }:
+    { pkgs, ... }:
     {
+      imports = [ self.modules.nixos.cloudflare-api ];
+
       clan.core.vars.generators = {
         vaultwarden-admin = {
           prompts.password-input = {
@@ -35,59 +38,63 @@ in
         };
       };
 
-      my.service-vms.vaultwarden.modules =
-        let
-          cfg = config;
-        in
-        [
-          (
-            { config, ... }:
-            {
-              microvm.volumes = [
-                {
-                  image = "vaultwarden-data.img";
-                  mountPoint = config.services.vaultwarden.config.DATA_FOLDER;
-                  size = 64;
-                }
+      my.service-vms.vaultwarden.modules = [
+        (
+          { config, hostConfig, ... }:
+          {
+            imports = with self.modules.nixos; [ nginx ];
+
+            my.nginx = {
+              inherit vHost;
+              port = config.services.vaultwarden.config.ROCKET_PORT;
+              proxyWebsockets = true;
+            };
+
+            microvm.volumes = [
+              {
+                image = "vaultwarden-data.img";
+                mountPoint = config.services.vaultwarden.config.DATA_FOLDER;
+                size = 64;
+              }
+            ];
+            microvm.credentialFiles = {
+              ADMIN_TOKEN = hostConfig.clan.core.vars.generators.vaultwarden-admin.files.password-hash.path;
+              SMTP_PASS = hostConfig.clan.core.vars.generators.koppe-development-smtp.files.password.path;
+            };
+
+            systemd.services.vaultwarden = {
+              serviceConfig.ImportCredential = [
+                "ADMIN_TOKEN"
+                "SMTP_PASS"
               ];
-              microvm.credentialFiles = {
-                ADMIN_TOKEN = cfg.clan.core.vars.generators.vaultwarden-admin.files.password-hash.path;
-                SMTP_PASS = cfg.clan.core.vars.generators.koppe-development-smtp.files.password.path;
+              environment = {
+                ADMIN_TOKEN_FILE = "%d/ADMIN_TOKEN";
+                SMTP_PASSWORD_FILE = "%d/SMTP_PASS";
               };
+            };
+            services.vaultwarden = {
+              enable = true;
+              dbBackend = "sqlite";
 
-              systemd.services.vaultwarden = {
-                serviceConfig.ImportCredential = [
-                  "ADMIN_TOKEN"
-                  "SMTP_PASS"
-                ];
-                environment = {
-                  ADMIN_TOKEN_FILE = "%d/ADMIN_TOKEN";
-                  SMTP_PASSWORD_FILE = "%d/SMTP_PASS";
-                };
+              config = {
+                DOMAIN = "https://${vHost}";
+                SIGNUPS_ALLOWED = false;
+                DATA_FOLDER = dataDir;
+
+                ROCKET_ADDRESS = "127.0.0.1";
+                ROCKET_PORT = 8222;
+                ROCKET_LOG = "critical";
+
+                SMTP_HOST = "smtp.gmail.com";
+                SMTP_PORT = 587;
+                SMTP_SECURITY = "starttls";
+                SMTP_FROM = "koppe.development@gmail.com";
+                SMTP_USERNAME = "koppe.development@gmail.com";
+                SMTP_FROM_NAME = "Koppe Vaultwarden";
               };
-              services.vaultwarden = {
-                enable = true;
-                dbBackend = "sqlite";
-
-                config = {
-                  DOMAIN = "https://${vHost}";
-                  SIGNUPS_ALLOWED = false;
-                  DATA_FOLDER = dataDir;
-
-                  ROCKET_ADDRESS = "127.0.0.1";
-                  ROCKET_PORT = 8222;
-                  ROCKET_LOG = "critical";
-
-                  SMTP_HOST = "smtp.gmail.com";
-                  SMTP_PORT = 587;
-                  SMTP_SECURITY = "starttls";
-                  SMTP_FROM = "koppe.development@gmail.com";
-                  SMTP_USERNAME = "koppe.development@gmail.com";
-                  SMTP_FROM_NAME = "Koppe Vaultwarden";
-                };
-              };
-            }
-          )
-        ];
+            };
+          }
+        )
+      ];
     };
 }
