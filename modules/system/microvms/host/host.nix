5446d5efb442ac021346404c64216fa0ee9dc9dc
ben <koppe.development@gmail.com>
2026-01-04 09:32:14 -0800

    Create alias for entering VMs with host openssh key

--- modules/system/microvms/host/host.nix
+++ modules/system/microvms/host/host.nix
@@ -1,10 +1,22 @@
 { self, inputs, ... }:
 {
-  flake.modules.nixos."microvms_host" = {
-    imports = with self.modules.nixos; [
-      inputs.microvm.nixosModules.host
+  flake.modules.nixos."microvms_host" =
+    { config, ... }:
+    {
+      imports = with self.modules.nixos; [
+        inputs.microvm.nixosModules.host
 
-      microvms_host_network
-    ];
-  };
+        microvms_host_network
+      ];
+
+      programs.bash.interactiveShellInit = ''
+        enter() {
+          if [ -z "$1" ]; then
+            echo "usage: enter <vm-name>" >&2
+            return 1
+          fi
+          microvm -s "$1" -- -i ${config.clan.core.vars.generators.openssh.files."ssh.id_ed25519".path}
+        }
+      '';
+    };
 }
