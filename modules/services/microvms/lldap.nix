26bbb292d43c1068588f34c2a5bf7f759d223f67
ben <koppe.development@gmail.com>
2026-01-03 23:43:28 -0800

    Correct lldap config for persistent state

--- modules/services/microvms/lldap.nix
+++ modules/services/microvms/lldap.nix
@@ -1,4 +1,4 @@
-{ self, lib, ... }:
+{ self, ... }:
 let
   vHost = "lldap2.thekoppe.com";
 in
@@ -34,7 +34,6 @@ in
               type = "hidden";
               persist = true;
             };
-            files.value.owner = "lldap";
             share = true;
           };
         in
@@ -46,46 +45,49 @@ in
 
       networking.firewall.allowedTCPPorts = [ 17170 ];
 
-      users = {
-        users.lldap = {
-          isNormalUser = true;
-          group = "lldap";
+      networking.firewall.extraCommands = ''
+        iptables -A INPUT -p tcp --dport 3890 -s 10.0.0.5 -j ACCEPT
+        iptables -A INPUT -p tcp --dport 3890 -j DROP
+      '';
+
+      systemd.services.lldap =
+        let
+          getSecret = name: config.clan.core.vars.generators.${name}.files.value.path;
+        in
+        {
+          serviceConfig.LoadCredential = [
+            ''key-seed:${getSecret "lldap-key-seed"}''
+            ''jwt-secret:${getSecret "lldap-jwt-secret"}''
+            ''user-pass:${getSecret "lldap-user-pass"}''
+          ];
         };
-
-        groups.lldap = { };
-      };
-
-      systemd.services.lldap.serviceConfig.DynamicUser = lib.mkForce false;
-
       services.lldap = {
         enable = true;
         silenceForceUserPassResetWarning = true;
 
-        settings =
-          let
-            getSecret = name: config.clan.core.vars.generators.${name}.files.value.path;
-          in
-          {
-            key_seed_file = getSecret "lldap-key-seed";
-            jwt_secret_file = getSecret "lldap-jwt-secret";
-            ldap_user_pass_file = getSecret "lldap-user-pass";
+        environment = {
+          LLDAP_KEY_SEED_FILE = "%d/key-seed";
+          LLDAP_JWT_SECRET_FILE = "%d/jwt-secret";
+          LLDAP_LDAP_USER_PASS_FILE = "%d/user-pass";
+        };
 
-            http_url = "https://${vHost}";
-            http_host = "0.0.0.0";
-            http_port = 17170;
+        settings = {
+          http_url = "https://${vHost}";
+          http_host = "0.0.0.0";
+          http_port = 17170;
 
-            ldap_host = "0.0.0.0";
-            ldap_port = 3890;
+          ldap_host = "0.0.0.0";
+          ldap_port = 3890;
 
-            force_ldap_user_pass_reset = false;
+          force_ldap_user_pass_reset = false;
 
-            ldap_user_dn = "ldap-admin";
-            ldap_user_email = "ldap-admin@thekoppe.com";
+          ldap_user_dn = "ldap-admin";
+          ldap_user_email = "ldap-admin@thekoppe.com";
 
-            ldap_base_dn = "dc=thekoppe,dc=com";
+          ldap_base_dn = "dc=thekoppe,dc=com";
 
-            database_url = "sqlite://./users.db?mode=rwc";
-          };
+          database_url = "sqlite://./users.db?mode=rwc";
+        };
       };
     };
 }
