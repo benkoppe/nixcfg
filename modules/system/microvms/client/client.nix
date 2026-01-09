83a39f6ae9f69c867944262d9abc48794b184022
ben <koppe.development@gmail.com>
2026-01-08 22:27:33 -0800

    Add journal-data.img

--- modules/system/microvms/client/client.nix
+++ modules/system/microvms/client/client.nix
@@ -26,6 +26,19 @@
       nixpkgs.hostPlatform = "x86_64-linux";
 
       clan.core.deployment.requireExplicitUpdate = true;
+
+      # see https://git.clan.lol/clan/clan-core/src/commit/ca83bb80c2ded7fe6f3a26216e62d35aa3158a59/nixosModules/clanCore/zfs.nix
+      # this resolves conflict between microvm.nix and zfs.nix
+      networking.hostId = "8425e349";
+
+      # persistent systemctl logs
+      microvm.volumes = [
+        {
+          image = "journal-data.img";
+          mountPoint = "/var/log/journal";
+          size = 1024; # 1 GiB
+        }
+      ];
     };
   };
 }
