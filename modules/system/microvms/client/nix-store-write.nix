1d4409cdd92b8c0176ef7d21ddb164645b574cf9
ben <koppe.development@gmail.com>
2026-01-09 03:29:57 -0800

    Auto delete nix-store-overlay images

--- modules/system/microvms/client/nix-store-write.nix
+++ modules/system/microvms/client/nix-store-write.nix
@@ -15,5 +15,12 @@
           fsType = "ext4";
         }
       ];
+
+      # the nix store will forget everything in the overlay on boot
+      # address this by just deleting the image each startup
+      # see: https://microvm-nix.github.io/microvm.nix/shares.html#writable-nixstore-overlay:~:text=writableStoreOverlay%3B%0A%20%20%20%20size%20%3D%202048%3B%0A%20%20%7D%20%5D%3B%0A%7D-,The%20Nix%20database,-will%20forget%20all
+      microvm.preStart = ''
+        rm -f nix-store-overlay.img
+      '';
     };
 }
