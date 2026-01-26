76a4cc171e1dbe2754e602d5277202e6212d612e
ben <koppe.development@gmail.com>
2026-01-26 08:32:56 -0500

    Setup ncps in inventory

--- flake.nix
+++ flake.nix
@@ -149,6 +149,34 @@
               roles.default.tags.all = { };
               roles.default.extraModules = [ ];
             };
+
+            # use dray as cache proxy
+            ncps = {
+              roles.server.machines."dray".settings = {
+                dataPath = "/var/lib/ncps";
+                caches = [
+                  "https://cache.nixos.org"
+                  "https://nix-community.cachix.org"
+                  "https://install.determinate.systems"
+                  "https://cache.saumon.network/proxmox-nixos"
+                  "https://nixpkgs-unfree.cachix.org"
+                  "https://nix-gaming.cachix.org"
+                  "https://cuda-maintainers.cachix.org"
+                ];
+                publicKeys = [
+                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                  "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+                  "proxmox-nixos:D9RYSWpQQC/msZUWphOY2I5RLH5Dd6yQcaHIuug7dWM="
+                  "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+                  "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+                  "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+                ];
+              };
+
+              roles.client.machines."luka".settings = { };
+              roles.client.machines."shai".settings = { };
+            };
           };
         };
       };
