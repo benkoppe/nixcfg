dee73daba467f366d02f2b260b61009c32da0924
ben <koppe.development@gmail.com>
2026-02-27 10:46:01 -0500

    Use determinateNix instead of determinate-nix

--- modules/darwin/programs/determinate.nix
+++ modules/darwin/programs/determinate.nix
@@ -16,51 +16,55 @@
       {
         nix.enable = false;
 
-        determinate-nix.customSettings = lib.mkMerge [
-          config.mySnippets.nix.settings
-          {
-            eval-cores = 0;
-          }
-          (
-            let
-              # convert normal nix buildMachines config to determinate string format
-              builderToString =
-                b:
-                let
-                  # safely get an attribute or fallback
-                  get =
-                    name: default:
-                    if builtins.hasAttr name b then
-                      let
-                        val = builtins.getAttr name b;
-                      in
-                      if val == "" || val == [ ] then default else val
-                    else
-                      default;
+        determinateNix = {
+          enable = true;
 
-                  hostName = get "hostName" "-";
-                  sshUser = get "sshUser" "-";
-                  protocol = get "protocol" "ssh";
-                  uri =
-                    if sshUser == "-" then "${protocol}://${hostName}" else "${protocol}://${sshUser}@${hostName}";
-
-                  systems = builtins.concatStringsSep "," (get "systems" [ "-" ]);
-                  sshKey = get "sshKey" "-";
-                  maxJobs = toString (get "maxJobs" "-");
-                  speedFactor = toString (get "speedFactor" "-");
-                  supportedFeatures = builtins.concatStringsSep "," (get "supportedFeatures" [ "-" ]);
-                  mandatoryFeatures = builtins.concatStringsSep "," (get "mandatoryFeatures" [ "-" ]);
-                  publicHostKey = get "publicHostKey" "-";
-                in
-                "${uri} ${systems} ${sshKey} ${maxJobs} ${speedFactor} ${supportedFeatures} ${mandatoryFeatures} ${publicHostKey}";
-            in
+          customSettings = lib.mkMerge [
+            config.mySnippets.nix.settings
             {
-              builders = builtins.concatStringsSep " ; " (
-                map builderToString config.mySnippets.nix.buildMachines
-              );
+              eval-cores = 0;
             }
-          )
-        ];
+            (
+              let
+                # convert normal nix buildMachines config to determinate string format
+                builderToString =
+                  b:
+                  let
+                    # safely get an attribute or fallback
+                    get =
+                      name: default:
+                      if builtins.hasAttr name b then
+                        let
+                          val = builtins.getAttr name b;
+                        in
+                        if val == "" || val == [ ] then default else val
+                      else
+                        default;
+
+                    hostName = get "hostName" "-";
+                    sshUser = get "sshUser" "-";
+                    protocol = get "protocol" "ssh";
+                    uri =
+                      if sshUser == "-" then "${protocol}://${hostName}" else "${protocol}://${sshUser}@${hostName}";
+
+                    systems = builtins.concatStringsSep "," (get "systems" [ "-" ]);
+                    sshKey = get "sshKey" "-";
+                    maxJobs = toString (get "maxJobs" "-");
+                    speedFactor = toString (get "speedFactor" "-");
+                    supportedFeatures = builtins.concatStringsSep "," (get "supportedFeatures" [ "-" ]);
+                    mandatoryFeatures = builtins.concatStringsSep "," (get "mandatoryFeatures" [ "-" ]);
+                    publicHostKey = get "publicHostKey" "-";
+                  in
+                  "${uri} ${systems} ${sshKey} ${maxJobs} ${speedFactor} ${supportedFeatures} ${mandatoryFeatures} ${publicHostKey}";
+              in
+              {
+                builders = builtins.concatStringsSep " ; " (
+                  map builderToString config.mySnippets.nix.buildMachines
+                );
+              }
+            )
+          ];
+        };
       }
 
       (lib.mkIf config.myDarwin.programs.determinate.accessTokens.enable {
@@ -79,7 +83,7 @@
           registryMap = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
         in
         {
-          determinate-nix.customSettings = {
+          determinateNix.customSettings = {
             flake-registry = "/etc/flake-registry.json";
             nix-path = lib.mapAttrsToList (name: flake: "${name}=${flake.outPath}") registryMap;
           };
