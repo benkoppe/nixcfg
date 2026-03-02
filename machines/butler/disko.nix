2588d5e212a7ca157dcb527b6132a3b43447c7d8
ben <koppe.development@gmail.com>
2026-03-02 06:24:43 -0500

    butler: disable grub

--- machines/butler/disko.nix
+++ machines/butler/disko.nix
@@ -1,15 +1,15 @@
 # ---
 # schema = "single-disk"
 # [placeholders]
-# mainDisk = "/dev/disk/by-id/nvme-VMware_Virtual_NVMe_Disk_VMware_NVME_0000" 
+# mainDisk = "/dev/disk/by-id/nvme-VMware_Virtual_NVMe_Disk_VMware_NVME_0000"
 # ---
 # This file was automatically generated!
 # CHANGING this configuration requires wiping and reinstalling the machine
 {
 
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.grub.enable = true;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.grub.enable = true;
   disko.devices = {
     disk = {
       main = {
