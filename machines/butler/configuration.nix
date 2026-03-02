ad0d9c8e921f6dd84ebf35cc468bfb7bd97b1aa0
ben <koppe.development@gmail.com>
2026-03-02 04:24:53 -0500

    Create starter config for butler

--- machines/butler/configuration.nix
+++ machines/butler/configuration.nix
@@ -1,7 +1,86 @@
 {
-  imports = [
+  self,
+  pkgs,
+  modulesPath,
+  ...
+}:
+{
+  imports = with self.modules.nixos; [
+    basics
 
+    "${modulesPath}/virtualisation/vmware-guest.nix"
+    "${modulesPath}/virtualisation/vmware-image.nix"
   ];
 
-  # New machine!
+  virtualisation.vmware.guest.enable = true;
+
+  # Setup qemu so we can run x86_64 binaries
+  boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
+
+  # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnsupportedSystem = true;
+
+  # Share our host filesystem
+  # fileSystems."/host" = {
+  #   fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
+  #   device = ".host:/";
+  #   options = [
+  #     "umask=22"
+  #     "uid=1000"
+  #     "gid=1000"
+  #     "allow_other"
+  #     "auto_unmount"
+  #     "defaults"
+  #   ];
+  # };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # VMware, Parallels both only support this being 0 otherwise you see
+  # "error switching console mode" on boot.
+  boot.loader.systemd-boot.consoleMode = "0";
+
+  # Define your hostname.
+  networking.hostName = "butler";
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+
+  # Don't require password for sudo
+  security.sudo.wheelNeedsPassword = false;
+
+  # Virtualization settings
+  # virtualisation.docker.enable = true;
+
+  # Select internationalisation properties. (copied from mitchellh)
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        qt6Packages.fcitx5-chinese-addons
+        fcitx5-gtk
+        fcitx5-hangul
+        fcitx5-mozc
+      ];
+    };
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = true;
+  services.openssh.settings.PermitRootLogin = "no";
+
+  # Disable the firewall since we're in a VM and we want to make it
+  # easy to visit stuff in here. We only use NAT networking anyways.
+  networking.firewall.enable = false;
 }
---
+++ machines/butler/hardware.nix
@@ -0,0 +1,28 @@
+# taken from mitchellh config
+{
+  imports = [ ];
+
+  boot.initrd.availableKernelModules = [
+    "uhci_hcd"
+    "ahci"
+    "xhci_pci"
+    "nvme"
+    "usbhid"
+    "sr_mod"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+  };
+
+  swapDevices = [ ];
+}
