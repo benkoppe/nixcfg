28d91a3b170019e92ed17a3b019b45cd7d6cbaa4
ben <koppe.development@gmail.com>
2026-03-02 21:31:57 -0500

    Implement ssh.nix for ben home

--- modules/home/ben/programs/ssh.nix
+++ modules/home/ben/programs/ssh.nix
@@ -1,3 +1,48 @@
 {
-  flake.modules.home."ben/ssh" = { };
+  flake.modules.home."ben/ssh" =
+    { config, ... }:
+    {
+      clan.core.vars.generators."github-ssh-key" = {
+        files."key" = {
+          secret = true;
+          owner = "ben";
+        };
+        files."key.pub" = {
+          secret = true;
+          owner = "ben";
+        };
+        script = ''
+          ssh-keygen -t ed25519 -N "" -C "" -f "$out"/key
+        '';
+        share = true;
+      };
+
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+
+        matchBlocks = {
+          "github.com" = {
+            identitiesOnly = true;
+            identityFile = [
+              config.clan.core.vars.generators."github-ssh-key".files."key".path
+            ];
+          };
+        };
+
+        # equivalent to enableDefaultConfig = true;
+        matchBlocks."*" = {
+          forwardAgent = false;
+          addKeysToAgent = "yes";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
+      };
+    };
 }
