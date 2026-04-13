9e5a73e1dc75228f62c44eac82c0ce040324b37b
ben <koppe.development@gmail.com>
2026-04-13 16:49:53 -0400

    Add helium to hammerspoon "new tab right/below" script

--- modules/darwin/programs/browsers.nix
+++ modules/darwin/programs/browsers.nix
@@ -39,6 +39,15 @@
               hs.window.filter.new("Brave Browser")
                 :subscribe(hs.window.filter.windowFocused, function() newWindowRight:enable() end)
                 :subscribe(hs.window.filter.windowUnfocused, function() newWindowRight:disable() end)
+
+              heliumNewBelow = hs.hotkey.new({"ctrl"}, "t", function()
+                local app = hs.appfinder.appFromName("Helium")
+                app:selectMenuItem({"Tabs", "New Tab Below"})
+              end)
+
+              hs.window.filter.new("Helium")
+                :subscribe(hs.window.filter.windowFocused, function() heliumNewBelow:enable() end)
+                :subscribe(hs.window.filter.windowUnfocused, function() heliumNewBelow:disable() end)
             '';
       })
     ];
