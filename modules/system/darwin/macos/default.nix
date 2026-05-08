5a16a64f2d8cb2474ca6a5a8943eb002d27c2fc3
Ben <ben@jordan.pug-economy.ts.net>
2026-05-08 11:52:01 -0400

    Add more macos system settings

--- modules/system/darwin/macos/default.nix
+++ modules/system/darwin/macos/default.nix
@@ -1,17 +1,58 @@
 { self, ... }:
 {
-  flake.modules.darwin.macos-defaults = {
-    imports = with self.modules.darwin; [
-      dock
-      finder
-      keybinds
-      keyboard
-      power
-      safari
-      screencapture
-      trackpad
-      unshittify
-      windowmanager
-    ];
-  };
+  flake.modules.darwin.macos-defaults =
+    { config, ... }:
+    {
+      imports = with self.modules.darwin; [
+        dock
+        finder
+        keybinds
+        keyboard
+        power
+        safari
+        screencapture
+        trackpad
+        unshittify
+        windowmanager
+      ];
+
+      security.pam.services.sudo_local = {
+        enable = true;
+        reattach = true;
+        touchIdAuth = true;
+      };
+
+      system = {
+        activationScripts.postActivation.text = ''
+          # should allow us to avoid a logout/login cycle when changing settings
+          sudo -u ${config.system.primaryUser} /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+        '';
+      };
+
+      # stop login print in shell
+      hjem.extraModules = [
+        {
+          files.".hushlogin".text = "";
+        }
+      ];
+
+      # DEBUG SERVER
+      # <https://sourcegraph.com/github.com/llvm/llvm-project@70906f0514826b5e64bd9354210ae836740c2053/-/blob/lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunication.cpp?L883>
+      environment.variables.LLDB_DEBUGSERVER_PATH = "/Library/Developer/CommandLineTools/Library/PrivateFrameworks/LLDB.framework/Versions/A/Resources/debugserver";
+
+      # Login window
+      system.defaults.loginwindow = {
+        DisableConsoleAccess = true;
+        GuestEnabled = false;
+      };
+
+      # Menu bar
+      system.defaults = {
+        menuExtraClock.Show24Hour = false;
+        menuExtraClock.ShowSeconds = false;
+
+        controlcenter.BatteryShowPercentage = true;
+        controlcenter.Bluetooth = true;
+      };
+    };
 }
