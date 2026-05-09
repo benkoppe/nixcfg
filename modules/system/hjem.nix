9ae73ce0d52fb13a9c1508a48a13228bafcbdc15
ben <koppe.development@gmail.com>
2026-05-08 20:17:22 -0400

    Force set XDG vars with hjem

--- modules/system/hjem.nix
+++ modules/system/hjem.nix
@@ -1,7 +1,10 @@
 { inputs, self, ... }:
 {
   flake.modules.generic.hjem = {
-    hjem.extraModules = [ inputs.hjem-rum.hjemModules.hjem-rum ];
+    hjem.extraModules = [
+      inputs.hjem-rum.hjemModules.hjem-rum
+      self.modules.hjem.hjem
+    ];
   };
 
   flake.modules.darwin.hjem = {
@@ -17,4 +20,20 @@
       self.modules.generic.hjem
     ];
   };
+
+  flake.modules.hjem.hjem =
+    { config, ... }:
+    {
+      # FORCE XDG ENV VARS
+      # hjem only exports XDG_*_HOME when config value != option default.
+      # The defaults do not match platform realities and setting the Linux
+      # defaults here causes env vars to not be set. Setting them directly
+      # bypasses hjem's conditional logic.
+      environment.sessionVariables = {
+        XDG_CACHE_HOME = "${config.directory}/.cache";
+        XDG_CONFIG_HOME = "${config.directory}/.config";
+        XDG_DATA_HOME = "${config.directory}/.local/share";
+        XDG_STATE_HOME = "${config.directory}/.local/state";
+      };
+    };
 }
