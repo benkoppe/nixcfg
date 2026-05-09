a7f978c5060db3aa29079b2daf13d51258d8d2f5
ben <koppe.development@gmail.com>
2026-05-08 22:57:09 -0400

    Add lazygit zsh alias

--- modules/system/programs/lazygit.nix
+++ modules/system/programs/lazygit.nix
@@ -6,6 +6,10 @@
         pkgs.lazygit
       ];
 
+      rum.programs.zsh.initConfig = lib.mkAfter ''
+        alias lg=lazygit
+      '';
+
       xdg.config.files."lazygit/config.yml" = {
         generator = lib.generators.toYAML { };
         value = {
