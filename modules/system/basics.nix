e29f466bbd6847c64a78afa68e27fd372ec3aa0e
ben <koppe.development@gmail.com>
2026-05-31 16:28:45 -0700

    Basics: add ghostty terminfo to nixos

--- modules/system/basics.nix
+++ modules/system/basics.nix
@@ -6,11 +6,15 @@
     };
   };
 
-  flake.modules.nixos.basics = {
-    imports = (with self.modules.generic; [ basics ]) ++ (with self.modules.nixos; [ nix ]);
+  flake.modules.nixos.basics =
+    { pkgs, ... }:
+    {
+      imports = (with self.modules.generic; [ basics ]) ++ (with self.modules.nixos; [ nix ]);
 
-    time.timeZone = lib.mkDefault "America/Los_Angeles";
-  };
+      time.timeZone = lib.mkDefault "America/Los_Angeles";
+
+      environment.systemPackages = [ pkgs.ghostty.terminfo ];
+    };
 
   flake.modules.darwin.basics = {
     imports = (with self.modules.generic; [ basics ]) ++ (with self.modules.darwin; [ nix ]);
