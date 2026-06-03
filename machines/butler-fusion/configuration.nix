ea60ef74cb064b6d7132d9525974c8c2a8269fc7
ben <koppe.development@gmail.com>
2026-06-03 12:32:38 -0700

    butler-fusion: initial working config

--- machines/butler-fusion/configuration.nix
+++ machines/butler-fusion/configuration.nix
@@ -1,7 +1,35 @@
 {
-  imports = [
+  self,
+  lib,
+  modulesPath,
+  ...
+}:
+{
+  imports = with self.modules.nixos; [
+    basics
+    development
 
+    # niri
+
+    "${modulesPath}/virtualisation/vmware-guest.nix"
   ];
 
-  # New machine!
+  virtualisation.vmware.guest.enable = true;
+
+  # automatically grow root partition to match disk
+  boot.growPartition = lib.mkDefault true;
+
+  # fixes booting on fusion
+  # without this, it would get stuck on "nixos-activation.service" until I spammed enter
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.consoleMode = "0";
+
+  # Setup qemu so we can run x86_64 binaries
+  boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
+
+  # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnsupportedSystem = true;
 }
