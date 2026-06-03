054aaccef5e48d033d5c374dbb5b6c2b4fe4fa76
ben <koppe.development@gmail.com>
2026-06-03 12:48:51 -0700

    niri: remove xserver config

--- modules/system/desktop/niri.nix
+++ modules/system/desktop/niri.nix
@@ -25,34 +25,5 @@
       };
 
       programs.niri.enable = true;
-
-      # services.xserver = {
-      #   enable = true;
-      #   xkb.layout = "us";
-      #   dpi = 220;
-      #
-      #   desktopManager = {
-      #     xterm.enable = false;
-      #     wallpaper.mode = "fill";
-      #   };
-      #
-      #   displayManager = {
-      #     lightdm.enable = true;
-      #
-      #     # AARCH64: For now, on Apple Silicon, we must manually set the
-      #     # display resolution. This is a known issue with VMware Fusion.
-      #     sessionCommands = ''
-      #       ${pkgs.xorg.xset}/bin/xset r rate 200 40
-      #     '';
-      #   };
-      #
-      #   windowManager = {
-      #     i3.enable = true;
-      #   };
-      # };
-      #
-      # services.displayManager = {
-      #   defaultSession = "none+i3";
-      # };
     };
 }
