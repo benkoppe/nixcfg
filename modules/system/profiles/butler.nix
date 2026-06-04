b2a77bb4005bbabb392b14fcb85a3017e8c7fad1
ben <koppe.development@gmail.com>
2026-06-03 17:27:16 -0700

    butler: allow peers to ssh into each other

--- modules/system/profiles/butler.nix
+++ modules/system/profiles/butler.nix
@@ -1,22 +1,61 @@
 { self, ... }:
 {
-  flake.modules.nixos.profiles_butler = {
-    imports = with self.modules.nixos; [
-      basics
+  flake.modules.nixos.profiles_butler =
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
+    let
+      inherit (config.networking) hostName;
 
-      hjem
-      niri
-    ];
+      butlerHosts = lib.filter (name: lib.hasPrefix "butler-" name) (
+        lib.attrNames self.clan.inventory.machines
+      );
 
-    hjem.users.ben = {
-      user = "ben";
-      directory = "/home/ben";
+      peerButlerHosts = lib.filter (name: name != hostName) butlerHosts;
 
-      imports = with self.modules.hjem; [
-        profiles_butler
+      peerButlerAuthorizedKeys = map (
+        name: builtins.readFile "${self}/vars/per-machine/${name}/ben-butler-ssh/id_ed25519.pub/value"
+      ) peerButlerHosts;
+    in
+    {
+      imports = with self.modules.nixos; [
+        basics
+
+        hjem
+        niri
       ];
+
+      hjem.users.ben = {
+        user = "ben";
+        directory = "/home/ben";
+
+        imports = with self.modules.hjem; [
+          profiles_butler
+        ];
+      };
+
+      clan.core.vars.generators.ben-butler-ssh = {
+        files."id_ed25519" = {
+          secret = true;
+          owner = "ben";
+          mode = "0600";
+        };
+        files."id_ed25519.pub" = {
+          secret = false;
+          owner = "ben";
+          mode = "0644";
+        };
+        runtimeInputs = [ pkgs.openssh ];
+        script = ''
+          ssh-keygen -t ed25519 -N "" -C "ben@${hostName}" -f "$out"/id_ed25519
+        '';
+      };
+
+      users.users.ben.openssh.authorizedKeys.keys = peerButlerAuthorizedKeys;
     };
-  };
 
   flake.modules.hjem.profiles_butler = {
     imports = with self.modules.hjem; [
