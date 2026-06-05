c86f01fb9307a45ebc4a9153b3fd87061806d2d6
ben <koppe.development@gmail.com>
2026-06-04 23:37:49 -0700

    butler: allow colmena key

--- modules/system/profiles/butler.nix
+++ modules/system/profiles/butler.nix
@@ -52,7 +52,9 @@
         '';
       };
 
-      users.users.ben.openssh.authorizedKeys.keys = peerButlerAuthorizedKeys;
+      users.users.ben.openssh.authorizedKeys.keys = peerButlerAuthorizedKeys ++ [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgiH4Iu1GUe9Hd40cSnQH94EHj0VmjXdbsaBED2WMHT colmena"
+      ];
     };
 
   flake.modules.hjem.profiles_butler =
