c2af192207792c096f60a075f5da8b411065cebe
ben <koppe.development@gmail.com>
2026-06-05 17:42:29 -0700

    dms-shell: configure app icons

--- modules/system/programs/dms-shell.nix
+++ modules/system/programs/dms-shell.nix
@@ -1,3 +1,4 @@
+{ self, ... }:
 {
   flake.modules.nixos.dms-shell = {
     programs.dms-shell = {
@@ -11,10 +12,31 @@
       # Core features
       enableSystemMonitoring = true; # System monitoring widgets (dgop)
       enableVPN = true; # VPN management widget
-      enableDynamicTheming = true; # Wallpaper-based theming (matugen)
+      enableDynamicTheming = false; # Wallpaper-based theming (matugen)
       enableAudioWavelength = true; # Audio visualizer (cava)
       enableCalendarEvents = true; # Calendar integration (khal)
       enableClipboardPaste = true; # Pasting from the clipboard history (wtype)
     };
+
+    environment.sessionVariables = {
+      QT_QPA_PLATFORMTHEME = "gtk3";
+    };
+
+    hjem.extraModules = with self.modules.hjem; [ app-icons ];
   };
+
+  # use with dms-shell to provide app icons
+  flake.modules.hjem.app-icons =
+    { pkgs, ... }:
+    {
+      packages = with pkgs; [
+        kdePackages.breeze-icons
+        hicolor-icon-theme
+      ];
+
+      xdg.config.files."gtk-3.0/settings.ini".text = ''
+        [Settings]
+        gtk-icon-theme-name=breeze-dark
+      '';
+    };
 }
