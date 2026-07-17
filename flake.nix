80daed42308d7bee2697fb2188421f70633bf009
ben <koppe.development@gmail.com>
2026-07-17 12:32:56 -0700

    flake: replace nixpkgs-unstable with nixpkgs-weekly
    Protects against supply chain attacks (chilled weekly releases)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Infrastructure dendritic clan configuration with flake-parts";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
